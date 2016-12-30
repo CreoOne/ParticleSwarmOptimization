779ef3ed4ddb6eeab7416372c360d7f5c9c6a384
creo <creo.one@gmail.com>
2016-12-30 22:02:50 +0100

    Added basic simulation with per click move.

--- ParticleSwarmOptimizationFront/Simple2DVisualisation.Designer.cs
+++ ParticleSwarmOptimizationFront/Simple2DVisualisation.Designer.cs
@@ -28,19 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.fitnessMap = new System.Windows.Forms.PictureBox();
+            this.bMove = new System.Windows.Forms.Button();
+            this.bRegenerateParticles = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessMap)).BeginInit();
             this.SuspendLayout();
+            // 
+            // fitnessMap
+            // 
+            this.fitnessMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fitnessMap.Location = new System.Drawing.Point(0, 41);
+            this.fitnessMap.Name = "fitnessMap";
+            this.fitnessMap.Size = new System.Drawing.Size(543, 340);
+            this.fitnessMap.TabIndex = 0;
+            this.fitnessMap.TabStop = false;
+            // 
+            // bMove
+            // 
+            this.bMove.Location = new System.Drawing.Point(12, 12);
+            this.bMove.Name = "bMove";
+            this.bMove.Size = new System.Drawing.Size(75, 23);
+            this.bMove.TabIndex = 1;
+            this.bMove.Text = "Move";
+            this.bMove.UseVisualStyleBackColor = true;
+            this.bMove.Click += new System.EventHandler(this.bMove_Click);
+            // 
+            // bRegenerateParticles
+            // 
+            this.bRegenerateParticles.Location = new System.Drawing.Point(93, 12);
+            this.bRegenerateParticles.Name = "bRegenerateParticles";
+            this.bRegenerateParticles.Size = new System.Drawing.Size(135, 23);
+            this.bRegenerateParticles.TabIndex = 2;
+            this.bRegenerateParticles.Text = "Regenerate Particles";
+            this.bRegenerateParticles.UseVisualStyleBackColor = true;
+            this.bRegenerateParticles.Click += new System.EventHandler(this.bRegenerateParticles_Click);
             // 
             // Simple2DVisualisation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 381);
+            this.Controls.Add(this.bRegenerateParticles);
+            this.Controls.Add(this.bMove);
+            this.Controls.Add(this.fitnessMap);
             this.Name = "Simple2DVisualisation";
             this.Text = "Simple2DVisualisation";
+            this.Shown += new System.EventHandler(this.Simple2DVisualisation_Shown);
+            this.ResizeEnd += new System.EventHandler(this.Simple2DVisualisation_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessMap)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox fitnessMap;
+        private System.Windows.Forms.Button bMove;
+        private System.Windows.Forms.Button bRegenerateParticles;
     }
 }
--- ParticleSwarmOptimizationFront/Simple2DVisualisation.cs
+++ ParticleSwarmOptimizationFront/Simple2DVisualisation.cs
@@ -16,13 +16,13 @@ namespace ParticleSwarmOptimizationFront
     {
         private ParticleManager<Vector2> ParticleManager;
         private Func<Vector2, double> FitnessFunction;
+        private Bitmap FitnessBitmap;
 
         public Simple2DVisualisation()
         {
             InitializeComponent();
 
-            FitnessFunction = (Vector2 model) => { return (Vector2.Zero - model).Length(); };
-            GenerateParticles(20);
+            FitnessFunction = (Vector2 model) => { return Math.Max(0, Math.Min(255-(new Vector2(50, 50) - model).Length(), 255)); };
         }
 
         public void GenerateParticles(int amount)
@@ -31,10 +31,83 @@ namespace ParticleSwarmOptimizationFront
             Random rng = new Random(DateTime.Now.Millisecond);
 
             foreach (int i in Enumerable.Range(0, amount))
-                particles[i] = new Vector2Particle(new Vector2(rng.Next(-10, 10), rng.Next(-10, 10)), FitnessFunction);
+                particles[i] = new Vector2Particle(new Vector2(rng.Next(0, fitnessMap.Width), rng.Next(0, fitnessMap.Height)), FitnessFunction);
 
-            ParticleManager<Vector2> manager = new ParticleManager<Vector2>(particles);
-            manager.FitnessPriority = FitnessPriorityEnum.Falling;
+            ParticleManager = new ParticleManager<Vector2>(particles);
+            ParticleManager.FitnessPriority = FitnessPriorityEnum.Rising;
+        }
+
+        private Bitmap GenerateFitnessBitmap()
+        {
+            Bitmap result = new Bitmap(fitnessMap.Width, fitnessMap.Height);
+
+            using (Graphics graphics = Graphics.FromImage(result))
+            {
+                float sampleSize = 5;
+
+                int ySamples = (int)Math.Ceiling(fitnessMap.Height / sampleSize) + 1;
+                int xSamples = (int)Math.Ceiling(fitnessMap.Width / sampleSize) + 1;
+
+                foreach (float y in Enumerable.Range(0, ySamples))
+                {
+                    foreach (float x in Enumerable.Range(0, xSamples))
+                    {
+                        int value = (int)FitnessFunction.Invoke(new Vector2(x * sampleSize, y * sampleSize));
+                        using (Brush color = new SolidBrush(Color.FromArgb(value, value, value)))
+                            graphics.FillRectangle(color, x * sampleSize, y * sampleSize, sampleSize, sampleSize);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public void RedrawFitnessMap()
+        {
+            if (FitnessBitmap != null)
+                FitnessBitmap.Dispose();
+
+            FitnessBitmap = GenerateFitnessBitmap();
+
+            if (ParticleManager == null)
+                return;
+
+            if (fitnessMap.Image != null)
+                fitnessMap.Image.Dispose();
+
+            fitnessMap.Image = new Bitmap(fitnessMap.Width, fitnessMap.Height);
+
+            using (Graphics graphics = Graphics.FromImage(fitnessMap.Image))
+            {
+                graphics.DrawImage(FitnessBitmap, 0, 0);
+
+                using (SolidBrush brush = new SolidBrush(Color.Red))
+                    foreach (Vector2Particle particle in ParticleManager.Particles)
+                        graphics.FillPie(brush, particle.Model.X, particle.Model.Y, 5, 5, 0, 360);
+            }
+        }
+
+        private void Simple2DVisualisation_ResizeEnd(object sender, EventArgs e)
+        {
+            RedrawFitnessMap();
+        }
+
+        private void Simple2DVisualisation_Shown(object sender, EventArgs e)
+        {
+            GenerateParticles(20);
+            RedrawFitnessMap();
+        }
+
+        private void bMove_Click(object sender, EventArgs e)
+        {
+            ParticleManager.MoveParticles();
+            RedrawFitnessMap();
+        }
+
+        private void bRegenerateParticles_Click(object sender, EventArgs e)
+        {
+            GenerateParticles(20);
+            RedrawFitnessMap();
         }
     }
 }
